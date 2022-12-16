11788ef207f3774a76f303061d349c4f4ebaab86
M7164 <mslohate@gmail.com>
2022-12-16 12:11:19 +0530

    BasicCommit
    Hi all ,
    here I have created the project on Github.com, now you can clone it and start your further work

---
+++ amstno/src/amstno/Practice1.java
@@ -0,0 +1,8 @@
+package amstno;
+
+public class Practice1 {
+public static void main(String[]args) {
+	System.out.println("Hello guys welcome to GitHub");
+	System.out.println("Here is how we followed the process and able to create a workspace on Github");
+}
+}
