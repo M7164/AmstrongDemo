fdb2c5146ba2ae8b54f2090fd01810307e47d641
M7164 <mslohate@gmail.com>
2022-12-16 12:23:29 +0530

    Update Practice1.java

--- amstno/src/amstno/Practice1.java
+++ amstno/src/amstno/Practice1.java
@@ -3,6 +3,25 @@ package amstno;
 public class Practice1 {
 public static void main(String[]args) {
 	System.out.println("Hello guys welcome to GitHub");
-	System.out.println("Here is how we followed the process and able to create a workspace on Github");
+	System.out.println("Here is how we followed the process and able to get into Github");
+	System.out.println("1. Open github desktop \r\n"
+			+ "2. Create new repo - File >> New Repo >> Provide name >> Path >> Click On create repo \r\n"
+			+ "3. Click on Publish repo - Then refresh github.com \r\n"
+			+ "\r\n"
+			+ "Obser - We are able to see created local repo on Github.com \r\n"
+			+ "\r\n"
+			+ "4. Open eclipse \r\n"
+			+ "5. Match path of local repo with our eclipse workspace \r\n"
+			+ "	File >> Switch workspace >> other >> choose path upto repo \r\n"
+			+ "6. Create project and complete your task \r\n"
+			+ "\r\n"
+			+ "7. Open Github Desktop \r\n"
+			+ "\r\n"
+			+ "Obser - We are able to see all changes that we have made eclipse are reflecting on github desktop \r\n"
+			+ "\r\n"
+			+ "8. Commit your code \r\n"
+			+ "9. Click On push origin\r\n"
+			+ "\r\n"
+			+ "Obser - We are able to see all changes on github.com now after push");
 }
 }
