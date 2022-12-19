6d17a8e235dc58cc4b9eb436e66b07f75e502461
M7164 <mslohate@gmail.com>
2022-12-19 11:17:29 +0530

    h
    i

--- amstno/src/amstno/Practice1.java
+++ amstno/src/amstno/Practice1.java
@@ -1,7 +1,7 @@
 package amstno;
 
 public class Practice1 {
-public static void main(String[]args) {
+public static void main (String[]args) {
 	System.out.println("Hello guys welcome to GitHub");
 	System.out.println("Here is how we followed the process and able to get into Github");
 	System.out.println("1. Open github desktop \r\n"
@@ -26,5 +26,44 @@ public static void main(String[]args) {
 	
 	
 	System.out.println("Note" +"\n"+ "Every time you make the changes in the code you have to commit  it and push or fetch again so as to get the changes in the github.com and every one in the team will be able to track the changes made");
+	System.out.println("Preaq -\r\n"
+			+ "1. eclipse\r\n"
+			+ "2. github.com\r\n"
+			+ "3. github desktop\r\n"
+			+ "------------------------\r\n"
+			+ "1. Open Github desktop\r\n"
+			+ "2. Create a new repo - File >> New Repo >> provide name >> choose path >> Create repo\r\n"
+			+ "3. click publish repo\r\n"
+			+ "Observation - We will able to see our created local repo on github.com\r\n"
+			+ "4. Open eclipse\r\n"
+			+ "5. match the path of workspace with local repo\r\n"
+			+ "6. Create project (task)\r\n"
+			+ "7. Open Github desktop\r\n"
+			+ "8. Commit your code to git (Local Repo)\r\n"
+			+ "9. click push origin\r\n"
+			+ "Observation - We will able to see our created projected with all changes on github.com\r\n"
+			+ "--------------------------------------------------------------------------------------\r\n"
+			+ "1. Login in Github.com - visit your repo\r\n"
+			+ "2. click on code\r\n"
+			+ "3. Open with Github desktop\r\n"
+			+ "4. Click on locate... provide path\r\n"
+			+ "5. Clone again\r\n"
+			+ "Obseravtion - We are able to see your project in local repo\r\n"
+			+ "6. Open eclipse\r\n"
+			+ "7. match the path of workspace with local repo\r\n"
+			+ "8. import project\r\n"
+			+ "9. Create a feature branch - in github desktop >> add branch\r\n"
+			+ "10. publish branch\r\n"
+			+ "Obseravtion - We are able to see featue branch on github.com with same code which we have in\r\n"
+			+ "main branch\r\n"
+			+ "11. Open eclipse and complete your task\r\n"
+			+ "12. Open Github desktop\r\n"
+			+ "13. Commit your code to git (Local Repo)\r\n"
+			+ "14. click push origin\r\n"
+			+ "Obseravtion - We are able to see our all code is in created branch\r\n"
+			+ "15. Raise PR on click of compare and Pull request\r\n"
+			+ "16. Wait till approve by coll\r\n"
+			+ "17. Merge branch with main\r\n"
+			+ "Observation - We are able to see our all code in main branch");
 }
 }
